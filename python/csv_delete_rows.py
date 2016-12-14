b92ccd374da54a319db57710baca62d566cd4f87
Christoph Broschinski <broschinski@uni-bielefeld.de>
2016-12-14 10:14:54 +0100

    Extended deletion script to read multiple deletion triggers from files

--- python/csv_delete_rows.py
+++ python/csv_delete_rows.py
@@ -5,6 +5,7 @@ import argparse
 import codecs
 import copy
 import csv
+import os
 import sys
 
 import openapc_toolkit as oat
@@ -12,12 +13,17 @@ import openapc_toolkit as oat
 ARG_HELP_STRINGS = {
     "csv_file": "The file to delete lines from",
     "index": "The index of the column to test for the delete condition",
-    "value": "The value on which to trigger a line deletion",
+    "value": "A single value to trigger a line deletion on. Either " +
+             "this argument or a file (using the -f option) must be given",
+    "file": "A file containing multiple values to trigger a line " +
+            "deletion on. Values must be given line by line, blank lines " +
+            "will be ignored. Either this argument or a single value " +
+            "(using the -v option) must be given",
     "full_delete": "Fully delete the line, reducing the total number " +
                    "of rows in the result file. Otherwise, the line " +
                    "is replaced by a row of emtpy values. ",
-    "deleted_entries_file": "Write deleted entries to a separate " + 
-                            "out file ('del.csv')",
+    "results_file": "Write deleted entries to a separate " + 
+                    "out file ('del.csv')",
     "encoding": "The encoding of the CSV file. Setting this argument will " +
                 "disable automatic guessing of encoding.",
     "quotemask": "A quotemask to apply to the result file after the action " +
@@ -37,9 +43,10 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("csv_file", help=ARG_HELP_STRINGS["csv_file"])
     parser.add_argument("index", type=int, help=ARG_HELP_STRINGS["index"])
-    parser.add_argument("value", help=ARG_HELP_STRINGS["value"])
+    parser.add_argument("-v", "--value", help=ARG_HELP_STRINGS["value"])
+    parser.add_argument("-f", "--file", help=ARG_HELP_STRINGS["file"])
     parser.add_argument("-d", "--full_delete", action="store_true", help=ARG_HELP_STRINGS["full_delete"])
-    parser.add_argument("-f", "--deleted_entries_file", action="store_true", help=ARG_HELP_STRINGS["deleted_entries_file"])
+    parser.add_argument("-r", "--results_file", action="store_true", help=ARG_HELP_STRINGS["results_file"])
     parser.add_argument("-e", "--encoding", help=ARG_HELP_STRINGS["encoding"])
     parser.add_argument("-q", "--quotemask", help=ARG_HELP_STRINGS["quotemask"])
     parser.add_argument("-o", "--openapc_quote_rules", 
@@ -47,6 +54,26 @@ def main():
                         action="store_true", default=False)
     
     args = parser.parse_args()
+    if not args.value and not args.file:
+        parser.error("Either a single value (-v option) or a file of " +
+                     "multiple values (-f option) must be given.")
+    
+    values = []
+    if args.file:
+        if not os.path.isfile(args.file):
+            print ("Error: '" + args.file + "' is no valid file!")
+            sys.exit() 
+        with open(args.file, "r") as f:
+            for line in f:
+                if len(line) > 0:
+                    values.append(line.strip("\r\n"))
+        oat.print_g(str(len(values)) + " values read from file")
+    
+    if args.value:
+        values.append(args.value)
+        if args.file:
+            oat.print_y("Value argument given in addition to file " +
+                        "argument, adding value to file imports...")
     
     quote_rules = args.openapc_quote_rules
     
@@ -82,7 +109,7 @@ def main():
         header_line = header[0]
         column_name = header_line[args.index]
         emtpy_line = [u'' for element in header_line]
-    msg = u"Performing line deletion on condition {} == {}".format(column_name, args.value)
+    msg = u"Performing line deletion on condition '{}' in {}".format(column_name, values)
     oat.print_g(msg)
     
     modified_content = []
@@ -90,13 +117,13 @@ def main():
     num_total_lines = num_deleted_lines = 0
     for line in content:
         num_total_lines += 1
-        if line[args.index] != args.value:
+        if line[args.index] not in values:
             modified_content.append(line)
         else:
             num_deleted_lines += 1
             if not args.full_delete:
                 modified_content.append(list(emtpy_line))
-            if args.deleted_entries_file:
+            if args.results_file:
                 deleted_lines.append(line)
             
     msg = u"Process complete, deleted {} out of {} total lines".format(num_deleted_lines, num_total_lines)            
@@ -106,7 +133,7 @@ def main():
         writer = oat.OpenAPCUnicodeWriter(out, mask, quote_rules, False)
         writer.write_rows(copy.deepcopy(header) + modified_content)
 
-    if args.deleted_entries_file and len(deleted_lines) > 0:
+    if args.results_file and len(deleted_lines) > 0:
         with open('del.csv', 'w') as out:
             writer = oat.OpenAPCUnicodeWriter(out, mask, quote_rules, False)
             writer.write_rows(copy.deepcopy(header) + deleted_lines)
