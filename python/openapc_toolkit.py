0b0d2acc2a1db4aadaad9696860d25110b6c6377
Christoph Broschinski <broschinski@uni-bielefeld.de>
2018-04-20 11:15:36 +0200

    desingations updated, wl extended

--- python/openapc_toolkit.py
+++ python/openapc_toolkit.py
@@ -1088,7 +1088,8 @@ def get_unified_journal_title(journal_full_title):
         "Journal of Management and Governance": "Journal of Management & Governance",
         "Journal für Verbraucherschutz und Lebensmittelsicherheit": "Journal of Consumer Protection and Food Safety",
         "Nanoscale Horiz.": "Nanoscale Horizons",
-        "BeitrÃ¤ge zur Algebra und Geometrie / Contributions to Algebra and Geometry": "Beiträge zur Algebra und Geometrie / Contributions to Algebra and Geometry" 
+        "BeitrÃ¤ge zur Algebra und Geometrie / Contributions to Algebra and Geometry": "Beiträge zur Algebra und Geometrie / Contributions to Algebra and Geometry",
+        "Energy Environ. Sci.": "Energy & Environmental Science"
     }
     return journal_mappings.get(journal_full_title, journal_full_title)
 
--- python/test/test_apc_csv.py
+++ python/test/test_apc_csv.py
@@ -68,7 +68,8 @@ JOURNAL_OWNER_CHANGED = {
     "1076-1551": ["The Feinstein Institute for Medical Research (North Shore LIJ Research Institute)", "Springer Nature"], # Molecular Medicine
     "1555-4309": ["Wiley-Blackwell", "Hindawi Publishing Corporation"], # Contrast Media & Molecular Imaging
     "2049-1115": ["HAU, Journal of Ethnographic Theory", "University of Chicago Press"], # HAU: Journal of Ethnographic Theory
-    "0197-6729": ["Wiley-Blackwell", "Hindawi Publishing Corporation"] # Journal of Advanced Transportation
+    "0197-6729": ["Wiley-Blackwell", "Hindawi Publishing Corporation"], # Journal of Advanced Transportation
+    "0094-8276": ["Wiley-Blackwell", "American Geophysical Union (AGU)"] # "Geophysical Research Letters"
 }
 
 # A whiltelist for denoting changes in journal full open access policy. ISSNs
