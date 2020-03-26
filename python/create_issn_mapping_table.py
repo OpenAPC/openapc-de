99341891baff281ce76b9043500af398cb56ef89
Christoph Broschinski <broschinski@uni-bielefeld.de>
2020-03-26 15:41:05 +0100

    script extended to create issn->publisher mappings as well

--- python/create_issn_mapping_table.py
+++ python/create_issn_mapping_table.py
@@ -3,7 +3,18 @@
 
 from csv import DictReader, unix_dialect, writer
 
-issn_map = {}
+from test import whitelists as wl
+
+mappings = {
+    "journal_full_title": {},
+    "publisher": {}
+}
+
+def _get_publisher_identity(publisher):
+    for entry in wl.PUBLISHER_IDENTITY:
+        if publisher in entry[1]:
+            publisher = entry[0][0]
+    return publisher
 
 for data_file in ["../data/apc_de.csv", "../data/transformative_agreements/transformative_agreements.csv"]:
     with open(data_file, "r") as handle:
@@ -13,17 +24,25 @@ for data_file in ["../data/apc_de.csv", "../data/transformative_agreements/trans
                 issn = line[field]
                 if issn == "NA":
                     continue
-                if issn not in issn_map:
-                    issn_map[issn] = line["journal_full_title"]
-                else:
-                    if issn_map[issn] != line["journal_full_title"]:
-                        msg = 'Inconsistency: ISSN {} maps to different journals: "{}" and "{}"'
-                        print(msg.format(issn, issn_map[issn], line["journal_full_title"]))
+                for map_type in mappings.keys():
+                    if map_type == "publisher":
+                        if issn in wl.JOURNAL_OWNER_CHANGED:
+                            continue
+                        value = _get_publisher_identity(line[map_type])
+                    else:
+                        value = line[map_type]
+                    if issn not in mappings[map_type]:
+                        mappings[map_type][issn] = value
+                    else:
+                        if mappings[map_type][issn] != value:
+                            msg = 'Inconsistency: ISSN {} maps to different {}s: "{}" and "{}"'
+                            print(msg.format(issn, map_type, mappings[map_type][issn], value))
 
-result = list(issn_map.items())
-result.sort(key=lambda x: x[1])
-with open("out.csv", "w") as out_file:
-    csv_writer = writer(out_file, dialect=unix_dialect)
-    csv_writer.writerow(["issn", "journal_full_title"])
-    csv_writer.writerows(result)
+for map_type in mappings.keys():
+    result = list(mappings[map_type].items())
+    result.sort(key=lambda x: x[1])
+    with open("issn_to_" +  map_type + ".csv", "w") as out_file:
+        csv_writer = writer(out_file, dialect=unix_dialect)
+        csv_writer.writerow(["issn", map_type])
+        csv_writer.writerows(result)
         
