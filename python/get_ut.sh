75a317db9f5cfe1e9a3ca0d020bc5bcca8e72ad6
cbroschinski <broschinski@uni-bielefeld.de>
2020-09-23 15:26:33 +0200

    adaptions to new fetch.pl

--- python/get_ut.sh
+++ python/get_ut.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 cd ..
-fab -R pub get_ut:python/out.csv,python/out.csv,false
+if [ "$1" = "refresh" ]; then
+    fab -R pub get_ut:python/out.csv,python/out.csv,true
+else
+    fab -R pub get_ut:python/out.csv,python/out.csv,false
+fi
 cd python
