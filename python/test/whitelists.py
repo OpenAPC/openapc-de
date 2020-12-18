123d156170c2231088c408b5b5f7820fe5d6bf62
cbroschinski <broschinski@uni-bielefeld.de>
2020-12-18 15:18:56 +0100

    wl extended

--- python/test/whitelists.py
+++ python/test/whitelists.py
@@ -257,5 +257,7 @@ JOURNAL_HYBRID_STATUS_CHANGED = [
 # A list of ISBNs which are exempt from the usual ISBN duplicate check. This is necessary for cases like
 # multivolumed books where each volume has the same ISBN but is listed separately.
 NON_DUPLICATE_ISBNS = [
-    "978-3-205-79673-2" # Enzyklopädie der slowen. Kulturgeschichte in Kärnten, Vol 1-3
+    "978-3-205-79673-2", # Enzyklopädie der slowen. Kulturgeschichte in Kärnten, Vol 1-3
+    "978-3-205-79545-2", # Erica Tietze-Conrat, Tagebücher, Vol 1 + 2,3
+    "978-3-7065-5326-1" # Regesten zur Geschichte der Juden in Österreich, Vol 3-4
 ]
