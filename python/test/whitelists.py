43a89428610f8d5c2911cc97b20980597fd63b8f
jbartlewski <julia.bartlewski@uni-bielefeld.de>
2022-11-22 14:55:05 +0100

    mappings + wl updated

--- python/test/whitelists.py
+++ python/test/whitelists.py
@@ -315,7 +315,8 @@ JOURNAL_OWNER_CHANGED = {
     "2047-4938": ["Institution of Engineering and Technology (IET)", "Wiley-Blackwell"], # IET Biometrics
     "2047-4946": ["Institution of Engineering and Technology (IET)", "Wiley-Blackwell"], # IET Biometrics (electronic)
     "2035-8377": ["PAGEPress Publications", "MDPI AG"], # Neurology International
-    "2521-3555": ["International Cardiovascular Forum Journal", "Wiley-Blackwell"] # JCSM Clinical Reports
+    "2521-3555": ["International Cardiovascular Forum Journal", "Wiley-Blackwell"], # JCSM Clinical Reports
+    "1107-0625": ["Zerbinis Medical Publications", "BAKIS Productions"] # Journal of BUON (J BUON)
 }
 
 # A whiltelist for denoting changes in journal full open access policy. ISSNs
