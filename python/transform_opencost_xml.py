fb75b31e870d48235a7bad55261b6fbed8d7ba6f
cbroschinski <broschinski@uni-bielefeld.de>
2023-08-02 11:14:28 +0200

    import institution names via ROR during transformation

--- python/transform_opencost_xml.py
+++ python/transform_opencost_xml.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 import argparse
-from csv import DictWriter
+import csv
 
 import openapc_toolkit as oat
 
@@ -15,10 +15,26 @@ articles = None
 with open(args.xml_file) as f:
     content = f.read()
     articles = oat.process_opencost_xml(content)
-    
-fieldnames = list(oat.OPENCOST_EXTRACTION_FIELDS.keys())
+
+ror_map = {}
+with open(oat.INSTITUTIONS_FILE, "r") as ins_file:
+    reader = csv.DictReader(ins_file)
+    for line in reader:
+        ror_map[line["ror_id"]] = line["institution"]
+
+for article in articles:
+    ror_id = article["institution_ror"]
+    if ror_id not in ror_map:
+        ror_request = oat.get_metadata_from_ror(ror_id)
+        if ror_request["success"]:
+            ror_map[ror_id] = ror_request["data"]["institution"]
+        else:
+            oat.print_r(ror_request["error_msg"])
+    article["institution"] = ror_map.get(ror_id, "NA")
+
+fieldnames = ["institution"] + list(oat.OPENCOST_EXTRACTION_FIELDS.keys())
 with open("out.csv", "w") as out:
-    writer = DictWriter(out, fieldnames)
+    writer = csv.DictWriter(out, fieldnames)
     writer.writeheader()
     for article in articles:
         writer.writerow(article)
