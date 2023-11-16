833da7eb11ff21238b8af07dbb5f29e5cf3e3d7d
cbroschinski <broschinski@uni-bielefeld.de>
2023-11-16 16:36:50 +0100

    create additional institutional out files

--- python/transform_opencost_xml.py
+++ python/transform_opencost_xml.py
@@ -3,8 +3,11 @@
 
 import argparse
 import csv
+import os
 
 import openapc_toolkit as oat
+
+TARGET_DIR = "opencost_out"
 
 parser = argparse.ArgumentParser()
 parser.add_argument("xml_files", nargs="+", help="One or more openCost XML files which should validate against the official openCost XSD schema")
@@ -37,8 +40,35 @@ for article in articles:
     article["institution"] = ror_map.get(ror_id, "")
 
 fieldnames = ["institution"] + list(oat.OPENCOST_EXTRACTION_FIELDS.keys())
-with open("out.csv", "w") as out:
-    writer = csv.DictWriter(out, fieldnames)
-    writer.writeheader()
+
+if not os.path.isdir(TARGET_DIR):
+    os.mkdir(TARGET_DIR)
+
+csv_writers = {}
+handles = []
+
+all_articles_path = os.path.join(TARGET_DIR, "all_articles.csv")
+
+with open(all_articles_path, "w") as out:
+    main_writer = csv.DictWriter(out, fieldnames)
+    main_writer.writeheader()
     for article in articles:
-        writer.writerow(article)
+        institution = article["institution"]
+        invoice_id = article["contract_invoice_id"]
+        pub_type = article["type"]
+        ins_name = institution.lower().replace(" ", "_")
+        if oat.has_value(invoice_id):
+            ins_name += "_DEAL"
+        elif pub_type == "book":
+            ins_name += "_BPC"
+        if ins_name not in csv_writers:
+            path = os.path.join(TARGET_DIR, ins_name + ".csv")
+            handle = open(path, "w")
+            handles.append(handle)
+            csv_writers[ins_name] = csv.DictWriter(handle, fieldnames)
+            csv_writers[ins_name].writeheader()
+        csv_writers[ins_name].writerow(article)
+        main_writer.writerow(article)
+
+for handle in handles:
+    handle.close()
