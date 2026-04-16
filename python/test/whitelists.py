be5e65312ff1ee64dbbb561fdf4ab1f3ef8f72ad
jbartlewski <julia.bartlewski@uni-bielefeld.de>
2026-04-16 17:07:14 +0200

    wl updated

--- python/test/whitelists.py
+++ python/test/whitelists.py
@@ -1146,6 +1146,7 @@ JOURNAL_HYBRID_STATUS_CHANGED = [
 CONFIRMED_SIMILAR_CONTRACTS_AMOUNTS = [
     "032e6b942_wiley2019deal_2023", # DEAL Wiley 2023, Alfred-Wegener-Institut
     "00613ak93_sage2024gac_2024", # Wuppertal U
+    "02wxx3e24_sage2024gac_2024", # Brandenburg University of Technology Cottbus-Senftenberg
 ]
 
 # A whitelist to identify titles which a shared by multiple journals. The list
