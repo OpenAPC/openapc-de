4d73e7cb2fbdb75bf87f990724c02eb0f2f0e7a8
cbroschinski <broschinski@uni-bielefeld.de>
2026-08-04 13:52:28 +0200

    update wl

--- python/test/whitelists.py
+++ python/test/whitelists.py
@@ -1316,6 +1316,7 @@ CONFIRMED_SIMILAR_CONTRACTS_AMOUNTS = [
     "04b9vrm74_sage2024gac_2024", # OTH Regensburg
     "04b9vrm74_sn2024deal_2025", # OTH Regensburg
     "001w7jn25_sn2020deal_2023", # Charité - Universitätsmedizin Berlin
+    "04qmmjx98_sage2021gac_2023", # Osnabrück U
 ]
 
 # A whitelist to identify titles which a shared by multiple journals. The list
